f4724519949aa1bd38e23211a608c899da234062
Amin Siddiqui <amin.s@codewalla.com>
2017-12-08 18:16:42 +0530

    - Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PoC: Xcode Server for CI
+
+https://developer.apple.com/library/content/documentation/IDEs/Conceptual/xcode_guide-continuous_integration
+
+https://medium.com/xcblog/xcode9-xcode-server-comprehensive-ios-continuous-integration-3613a7973b48
---
+++ XcodeCIServerPoCTests/XcodeCIServerPoCTests.swift
@@ -0,0 +1,18 @@
+//
+//  XcodeCIServerPoCTests.swift
+//  XcodeCIServerPoCTests
+//
+//  Created by Amin Siddiqui on 08/12/17.
+//  Copyright © 2017 svmLogics. All rights reserved.
+//
+
+import XCTest
+@testable import XcodeCIServerPoC
+
+class XcodeCIServerPoCTests: XCTestCase {
+    
+    func testHasEnvirnoment() {
+        
+    }
+    
+}
